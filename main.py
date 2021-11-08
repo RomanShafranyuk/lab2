9ef1c46dc3c5f45ecffc2d5f1839ef3d1a5f233a
RomanShafranyuk <romanshafranuk@gmail.com>
2021-11-08 15:56:58 +0400

    refactoring

--- main.py
+++ main.py
@@ -15,8 +15,7 @@ output_path = args.output
 
 
 class File:
-    """
-    Объект класса File обрабатывает запись о научном сотруднике.
+    """Объект класса File обрабатывает запись о научном сотруднике.
 
     Используется для хранения полей записи, а также их валидации.
 
@@ -26,158 +25,129 @@ class File:
             Словарь хранит записи в виде "тип данных о сотруднике": данные о сотруднике.
     """
     inf: dict
-    """
-    Инициализирует экземпляр класса записи.
-
-    Parameters
-    ----------
-        d : dict
-            Копия списка с полями записи.
-
-    """
 
     def __init__(self, d) -> None:
+        """Инициализирует экземпляр класса записи.
+
+        Parameters
+        ----------
+            d : dict
+                Копия списка с полями записи.
+        """
         self.inf = d.copy()
 
-    """
-       Проверяет электронную почту на валидность.
-
-       Returns
-       -------
-           bool:
-               Результат проверки на корректность.
-
-       """
-
     def check_email(self) -> bool:
+        """Проверяет электронную почту на валидность.
+
+        Returns
+        -------
+            bool:
+                Результат проверки на корректность.
+        """
         pattern = "^[^\s@]+@([^\s@.,]+\.)+[^\s@.,]{2,}$"
         if re.match(pattern, self.inf["email"]):
             return True
         return False
 
-    """
-           Проверяет значение веса на валидность.
-
-           Returns
-           -------
-               bool:
-                   Результат проверки на корректность.
-
-           """
-
     def check_weight(self) -> bool:
+        """Проверяет значение веса на валидность.
+
+        Returns
+        -------
+            bool:
+                Результат проверки на корректность.
+        """
         if isinstance(self.inf["weight"], int):
             if 30 < self.inf["weight"] < 200:
                 return True
         return False
 
-    """
-           Проверяет номер СНИЛСа на валидность.
-
-           Returns
-           -------
-               bool:
-                   Результат проверки на корректность.
-
-           """
-
     def check_snils(self) -> bool:
+        """Проверяет номер СНИЛСа на валидность.
+
+        Returns
+        -------
+            bool:
+                Результат проверки на корректность.
+        """
         pattern = "^\d{11}$"
         if re.match(pattern, self.inf["snils"]):
             return True
         return False
 
-    """
-           Проверяет номер паспорта на валидность.
-
-           Returns
-           -------
-               bool:
-                   Результат проверки на корректность.
-
-           """
-
     def check_passport_number(self) -> bool:
+        """Проверяет номер паспорта на валидность.
+
+        Returns
+        -------
+            bool:
+                Результат проверки на корректность.
+        """
         if isinstance(self.inf["passport_number"], int):
             if 100000 <= self.inf["passport_number"] < 1000000:
                 return True
         return False
 
-    """
-           Проверяет название университета на валидность.
-
-           Returns
-           -------
-               bool:
-                   Результат проверки на корректность.
-
-           """
-
     def check_university(self) -> bool:
+        """Проверяет название университета на валидность.
+
+        Returns
+        -------
+            bool:
+                Результат проверки на корректность.
+        """
         pattern = "^.*(?:[Тт]ех|[Уу]нивер|[Аа]кадем|[Ии]нститут|им\.|СПбГУ|МФТИ|МГ(?:Т|)У).*$"
         if re.match(pattern, self.inf["university"]):
             return True
         return False
 
-    """
-           Проверяет значение опыта работы на валидность.
-
-           Returns
-           -------
-               bool:
-                   Результат проверки на корректность.
-
-           """
-
     def check_work_experience(self) -> bool:
+        """Проверяет значение опыта работы на валидность.
+
+        Returns
+        -------
+            bool:
+                Результат проверки на корректность.
+        """
         if isinstance(self.inf["work_experience"], int):
             if 0 <= self.inf["work_experience"] < 80:
                 return True
         return False
 
-    """
-           Проверяет значение академической степени на валидность.
-
-           Returns
-           -------
-               bool:
-                   Результат проверки на корректность.
-
-           """
-
     def check_academic_degree(self) -> bool:
+        """Проверяет значение академической степени на валидность.
+
+        Returns
+        -------
+            bool:
+                Результат проверки на корректность.
+        """
         pattern = "Бакалавр|Кандидат наук|Специалист|Магистр|Доктор наук|"
         if re.match(pattern, self.inf["academic_degree"]):
             return True
         return False
 
-    """
-           Проверяет взгляд на мир на валидность.
-
-           Returns
-           -------
-               bool:
-                   Результат проверки на корректность.
-
-           """
-
     def check_worldview(self) -> bool:
+        """Проверяет взгляд на мир на валидность.
+
+        Returns
+        -------
+            bool:
+                Результат проверки на корректность.
+        """
         pattern = "^.+(?:изм|анство)$"
         if re.match(pattern, self.inf["worldview"]):
             return True
         return False
 
-    """
-           Проверяет адрес на валидность.
-
-           Returns
-           -------
-               bool:
-                   Результат проверки на корректность.
-
-           """
-
     def check_address(self) -> bool:
+        """Проверяет адрес на валидность.
+
+        Returns
+        -------
+            bool:
+                Результат проверки на корректность.
+        """
         pattern = "(?:ул\.|Аллея) (?:им[\.\s]|)[^\s]+ [^\s]*(?:\s|)\d+"
         if re.match(pattern, self.inf["address"]):
             return True
@@ -185,47 +155,44 @@ class File:
 
 
 class Validator:
+    """Объект класса Validator проверяет записи файла на валидность.
+
+    Используется для сохранения валидных записей в файл и выдает результаты проверки.
+
+    Attributes
+    ----------
+        data : dict
+             Список записей.
     """
-       Объект класса Validator проверяет записи файла на валидность.
-
-       Используется для сохранения валидных записей в файл и выдает результаты проверки.
-
-       Attributes
-       ----------
-           data : dict
-                Список записей.
-       """
     data: list
-    """
-        Инициализирует экземпляр класса валидатор.
+
+    def __init__(self, inp) -> None:
+        """Инициализирует экземпляр класса валидатор.
 
         Parameters
         ----------
             inp : str
                 Путь к входному файлу с данными.
         """
-
-    def __init__(self, inp) -> None:
         self.data = []
         tmp = json.load(open(inp, encoding="windows-1251"))
         for i in tmp:
             self.data.append(File(i.copy()))
 
-    """
-    Выполняет валидацию записи по ее ключу.
-    Attributes
-    ----------
-        index : index
-            Индекс записи в списке записей
-
-    Returns
-    -------
-        dict:
-            Словарь вида: "вид данных о сотруднике":флаг, валидно ли значение.
-
-    """
-
     def validation(self, index) -> dict:
+        """
+        Выполняет валидацию записи по ее ключу.
+        Attributes
+        ----------
+            index : index
+                Индекс записи в списке записей
+
+        Returns
+        -------
+            dict:
+                Словарь вида: "вид данных о сотруднике":флаг, валидно ли значение.
+
+        """
         result = {"email": self.data[index].check_email(), "weight": self.data[index].check_weight(),
                   "snils": self.data[index].check_snils(), "passport_number": self.data[index].check_passport_number(),
                   "university": self.data[index].check_university(),
@@ -234,50 +201,45 @@ class Validator:
                   "worldview": self.data[index].check_worldview(), "adress": self.data[index].check_address()}
         return result.copy()
 
-    """
-               Считает число валидных записей.
-
-               Returns
-               -------
-                   int:
-                       Число валидных записей.
-
-               """
-
     def count_valid_records(self) -> int:
+        """
+        Считает число валидных записей.
+
+        Returns
+        -------
+            int:
+                Число валидных записей.
+        """
         count_correct = 0
         for i in tqdm(range(len(self.data)), desc="Подсчёт корректных записей", ncols=100):
             if not (False in self.validation(i).values()):
                 count_correct += 1
         return count_correct
 
-    """
-               Считает число невалидных записей.
-
-               Returns
-               -------
-                   int:
-                       Число невалидных записей.
-
-               """
-
     def count_invalid_records(self) -> int:
+        """
+        Считает число невалидных записей.
+
+        Returns
+        -------
+            int:
+                Число невалидных записей.
+        """
         count_incorrect = 0
         for i in tqdm(range(len(self.data)), desc="Подсчёт некорректных записей", ncols=100):
             if False in self.validation(i).values():
                 count_incorrect += 1
         return count_incorrect
 
-    """
-    Выполняет валидацию записи по ее ключу.
-    Attributes
-    ----------
-        output : str
-            Путь к выходному файлу
-
-    """
-
     def result_file(self, output) -> None:
+        """
+            Выполняет валидацию записи по ее ключу.
+            Attributes
+            ----------
+                output : str
+                    Путь к выходному файлу
+
+            """
         tmp = []
         for i in tqdm(range(len(self.data)), desc="Запись результата валидации в файл", ncols=100):
             if not (False in self.validation(i).values()):
@@ -285,17 +247,17 @@ class Validator:
         json.dump(tmp, open(output, "w", encoding="windows-1251"),
                   ensure_ascii=False, sort_keys=False, indent=4)
 
-    """
-   Считает число невалидных данных: электронная почта, вес, СНИЛС, номер паспорта, университет, опыт работы,
-   академическая степень, взгляд на мир, адрес.
-
-   Returns
-   -------
-       lst:
-           Список с количеством невалидных парметров.
-                   """
-
     def count_invalid_arguments(self):
+        '''
+        Считает число невалидных данных: электронная почта, вес, СНИЛС,
+        номер паспорта, университет, опыт работы, академическая степень, взгляд
+        на мир, адрес.
+
+        Returns
+        -------
+            lst:
+                Список с количеством невалидных парметров.
+        '''
         lst_res = []
         count_inv_email = 0
         count_inv_weight = 0
